bd7755020147db20bb3447ab1162a8ea4276db8b
shugah1 <60858525+shugah1@users.noreply.github.com>
2021-03-05 12:15:14 -0500

    Add files via upload

---
+++ test/desktop/src/com/hush/game/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.hush.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hush.game.main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new main(), config);
+
+
+
+	}
+}
