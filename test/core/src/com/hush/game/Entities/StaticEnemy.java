46fd676a0515a50b6e6cd8a03553e3884f55ec3d
ScorMarby <stupp.acosta@gmail.com>
2021-05-16 14:49:02 -0400

    Static Enemy with da funny

--- test/core/src/com/hush/game/Entities/StaticEnemy.java
+++ test/core/src/com/hush/game/Entities/StaticEnemy.java
@@ -1,6 +1,8 @@
 package com.hush.game.Entities;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.World;
 import com.hush.game.Main;
@@ -8,16 +10,21 @@ import com.hush.game.UI.Settings;
 
 public class StaticEnemy extends Enemy{
 
-    Texture image = new Texture("test/core/assets/badlogic.jpg");
+    private Animation<TextureRegion> idle;
+    private float elapsedTime = 0f;
 
     public StaticEnemy(World world, Main screen, float x, float y) {
         super(world, screen, x, y);
-        setRegion(image);
+        TextureAtlas ta = new TextureAtlas("Sprites/enemies.atlas");
+        idle = new Animation<TextureRegion>(1/5f, ta.findRegions("kevin_idle"), Animation.PlayMode.LOOP);
+        setRegion(idle.getKeyFrame(elapsedTime,true));
     }
 
     @Override
     public void update(float dt) {
         super.update(dt);
-        //setBounds(b2body.getPosition().x - getWidth() / Settings.PPM / 2f, b2body.getPosition().y - getHeight() / Settings.PPM / 2f, getWidth() / Settings.PPM, getHeight() / Settings.PPM);
+        elapsedTime += dt;
+        setRegion(idle.getKeyFrame(elapsedTime,true));
+        setBounds(b2body.getPosition().x - getRegionWidth() / Settings.PPM / 2f, b2body.getPosition().y - getRegionHeight() / Settings.PPM / 2f, getRegionWidth() / Settings.PPM, getRegionHeight() / Settings.PPM);
     }
 }
